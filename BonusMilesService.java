4bd32ab42328d33d049fcb730d089d86bd4e6436
Netotest <athomson@mail.ru>
2024-04-27 13:31:21 +0300

    com 3

--- BonusMilesService.java
+++ BonusMilesService.java
@@ -1,10 +1,10 @@
 public class BonusMilesService {
 
 
-    public int calculate (int price) {
+    public int calculate(int price) {
         int result;
         result = price / 20;
-    return result;
+        return result;
 
     }
 }
